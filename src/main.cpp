a70f87345a82f2141cfaeb8b2fa32afd8a1deb3d
Guy Barnard <guy@domabo.com>
2017-12-09 19:50:12 -0500

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -18,17 +18,13 @@ void ssd1306_splash(Adafruit_SSD1306 *ssd, void *data) {
   uint8_t * data_c = reinterpret_cast<uint8_t *>(data);
   int h = mgos_ssd1306_height(ssd);
   int w = mgos_ssd1306_width(ssd);
-  ssd->clearDisplay();
   ssd->drawBitmap(0, 0, data_c, w, h, WHITE);
-  ssd->display();
 }
 
 void ssd1306_drawBitmap(Adafruit_SSD1306 *ssd, void *data, int x, int y, int w, int h) {
   if (ssd == nullptr) return;
   uint8_t * data_c = reinterpret_cast<uint8_t *>(data);
-  ssd->clearDisplay();
   ssd->drawBitmap(x, y, data_c, w, h, WHITE);
-  ssd->display();
 }
 
 #ifdef __cplusplus
